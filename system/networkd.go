7f091aba403a864cb9c6523f25b04ade2e8e4416
Alex Crawford <alex.crawford@coreos.com>
2014-06-30 13:37:09 -0700

    bonding: Add support for probing the bonding module with parameters
    Until support for bonding params is added to networkd, this will be
    neccessary in order to use bonding parameters (i.e. miimon, mode).
    This also makes it such that the 8012q module will only be loaded if
    the network config makes use of VLANs.

--- system/networkd.go
+++ system/networkd.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"os/exec"
 	"path"
+	"strings"
 
 	"github.com/coreos/coreos-cloudinit/network"
 	"github.com/coreos/coreos-cloudinit/third_party/github.com/dotcloud/docker/pkg/netlink"
@@ -26,9 +27,13 @@ func RestartNetwork(interfaces []network.InterfaceGenerator) (err error) {
 		return
 	}
 
-	if err = probe8012q(); err != nil {
+	if err = probe8012q(interfaces); err != nil {
 		return
 	}
+	if err = probeBonding(interfaces); err != nil {
+		return
+	}
+
 	return
 }
 
@@ -55,8 +60,24 @@ func downNetworkInterfaces(interfaces []network.InterfaceGenerator) error {
 	return nil
 }
 
-func probe8012q() error {
-	return exec.Command("modprobe", "8021q").Run()
+func probe8012q(interfaces []network.InterfaceGenerator) error {
+	for _, iface := range interfaces {
+		if iface.Type() == "vlan" {
+			return exec.Command("modprobe", "8021q").Run()
+		}
+	}
+	return nil
+}
+
+func probeBonding(interfaces []network.InterfaceGenerator) error {
+	args := []string{"bonding"}
+	for _, iface := range interfaces {
+		if iface.Type() == "bond" {
+			args = append(args, strings.Split(iface.ModprobeParams(), " ")...)
+			break
+		}
+	}
+	return exec.Command("modprobe", args...).Run()
 }
 
 func restartNetworkd() error {
