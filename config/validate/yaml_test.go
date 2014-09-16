2b112264bad37c5c76a3a81be3fe2723ce641e69
Alex Crawford <alex.crawford@coreos.com>
2014-09-16 16:22:01 -0700

    more tests

--- config/validate/yaml_test.go
+++ config/validate/yaml_test.go
@@ -30,75 +30,151 @@ func TestSyntax(t *testing.T) {
 }
 
 func TestNodes(t *testing.T) {
-	for _, tt := range []struct {
+	for i, tt := range []struct {
 		c string
 		e []Entry
 	}{
 		{},
+
+		// Test for unrecognized keys
 		{
 			c: "test:",
 			e: []Entry{{entryWarning, "unrecognized key \"test\"", 1}},
 		},
 		{
-			c: "hostname:",
-			e: []Entry{{entryWarning, "incorrect type for \"hostname\" (want string)", 1}},
-		},
-		{
-			c: "hostname:\n  - bad",
-			e: []Entry{{entryWarning, "incorrect type for \"hostname\" (want string)", 1}},
-		},
-		{
-			c: "hostname: 4",
-		},
-		{
-			c: "coreos:\n  etcd:\n    discover:",
-			e: []Entry{{entryWarning, "unrecognized key \"discover\"", 3}},
+			c: "coreos:\n  etcd:\n    bad:",
+			e: []Entry{{entryWarning, "unrecognized key \"bad\"", 3}},
 		},
 		{
 			c: "coreos:\n  etcd:\n    discovery: good",
 		},
+
+		// Test for incorrect types
+		// Want boolean
 		{
-			c: "ssh_authorized_keys:\n  bad",
-			e: []Entry{{entryWarning, "incorrect type for \"ssh_authorized_keys\" (want []string)", 1}},
+			c: "coreos:\n  units:\n    - enable: true",
 		},
 		{
-			c: "ssh_authorized_keys:\n  - good\n  - 2",
-		},
-		{
-			c: "ssh_authorized_keys:\n  - good",
-		},
-		{
-			c: "users:\n  bad",
-			e: []Entry{{entryWarning, "incorrect type for \"users\" (want []struct)", 1}},
-		},
-		{
-			c: "users:\n  - bad",
-			e: []Entry{{entryWarning, "incorrect type for \"users\" (want struct)", 1}},
-		},
-		{
-			c: "users:\n  - name: good",
-		},
-		{
-			c: "coreos:\n  units:\n    - bad",
-			e: []Entry{{entryWarning, "incorrect type for \"units\" (want struct)", 2}},
-		},
-		{
-			c: "coreos:\n  units:\n    - name:\n      - bad",
-			e: []Entry{{entryWarning, "incorrect type for \"name\" (want string)", 3}},
+			c: "coreos:\n  units:\n    - enable: 4",
+			e: []Entry{{entryWarning, "incorrect type for \"enable\" (want bool)", 3}},
 		},
 		{
 			c: "coreos:\n  units:\n    - enable: bad",
 			e: []Entry{{entryWarning, "incorrect type for \"enable\" (want bool)", 3}},
 		},
 		{
-			c: "coreos:\n  units:\n    - name: test.service\n    - enable: true",
+			c: "coreos:\n  units:\n    - enable:\n        bad:",
+			e: []Entry{{entryWarning, "incorrect type for \"enable\" (want bool)", 3}},
+		},
+		{
+			c: "coreos:\n  units:\n    - enable:\n      - bad",
+			e: []Entry{{entryWarning, "incorrect type for \"enable\" (want bool)", 3}},
+		},
+		// Want string
+		{
+			c: "hostname: true",
+		},
+		{
+			c: "hostname: 4",
+		},
+		{
+			c: "hostname: host",
+		},
+		{
+			c: "hostname:\n  name:",
+			e: []Entry{{entryWarning, "incorrect type for \"hostname\" (want string)", 1}},
+		},
+		{
+			c: "hostname:\n  - name",
+			e: []Entry{{entryWarning, "incorrect type for \"hostname\" (want string)", 1}},
+		},
+		// Want struct
+		{
+			c: "coreos: true",
+			e: []Entry{{entryWarning, "incorrect type for \"coreos\" (want struct)", 1}},
+		},
+		{
+			c: "coreos: 4",
+			e: []Entry{{entryWarning, "incorrect type for \"coreos\" (want struct)", 1}},
+		},
+		{
+			c: "coreos: hello",
+			e: []Entry{{entryWarning, "incorrect type for \"coreos\" (want struct)", 1}},
+		},
+		{
+			c: "coreos:\n  etcd:\n    discovery: fire in the disco",
+		},
+		{
+			c: "coreos:\n  - hello",
+			e: []Entry{{entryWarning, "incorrect type for \"coreos\" (want struct)", 1}},
+		},
+		// Want []string
+		{
+			c: "ssh_authorized_keys: true",
+			e: []Entry{{entryWarning, "incorrect type for \"ssh_authorized_keys\" (want []string)", 1}},
+		},
+		{
+			c: "ssh_authorized_keys: 4",
+			e: []Entry{{entryWarning, "incorrect type for \"ssh_authorized_keys\" (want []string)", 1}},
+		},
+		{
+			c: "ssh_authorized_keys: key",
+			e: []Entry{{entryWarning, "incorrect type for \"ssh_authorized_keys\" (want []string)", 1}},
+		},
+		{
+			c: "ssh_authorized_keys:\n  key: value",
+			e: []Entry{{entryWarning, "incorrect type for \"ssh_authorized_keys\" (want []string)", 1}},
+		},
+		{
+			c: "ssh_authorized_keys:\n  - key",
+		},
+		{
+			c: "ssh_authorized_keys:\n  - key: value",
+			e: []Entry{{entryWarning, "incorrect type for \"ssh_authorized_keys\" (want []string)", 1}},
+		},
+		// Want []struct
+		{
+			c: "users:\n  true",
+			e: []Entry{{entryWarning, "incorrect type for \"users\" (want []struct)", 1}},
+		},
+		{
+			c: "users:\n  4",
+			e: []Entry{{entryWarning, "incorrect type for \"users\" (want []struct)", 1}},
+		},
+		{
+			c: "users:\n  bad",
+			e: []Entry{{entryWarning, "incorrect type for \"users\" (want []struct)", 1}},
+		},
+		{
+			c: "users:\n  bad:",
+			e: []Entry{{entryWarning, "incorrect type for \"users\" (want []struct)", 1}},
+		},
+		{
+			c: "users:\n  - name: good",
+		},
+		// Want struct within array
+		{
+			c: "users:\n  - true",
+			e: []Entry{{entryWarning, "incorrect type for \"users\" (want struct)", 1}},
+		},
+		{
+			c: "users:\n  - 4",
+			e: []Entry{{entryWarning, "incorrect type for \"users\" (want struct)", 1}},
+		},
+		{
+			c: "users:\n  - bad",
+			e: []Entry{{entryWarning, "incorrect type for \"users\" (want struct)", 1}},
+		},
+		{
+			c: "users:\n  - - bad",
+			e: []Entry{{entryWarning, "incorrect type for \"users\" (want struct)", 1}},
 		},
 	} {
 		v := validator{report: &Report{}}
 		nodes(context{content: []byte(tt.c)}, &v)
 
 		if e := v.report.Entries(); !reflect.DeepEqual(tt.e, e) {
-			t.Fatalf("bad report (%s): want %#v, got %#v", tt.c, tt.e, e)
+			t.Fatalf("bad report (%d, %s): want %#v, got %#v", i, tt.c, tt.e, e)
 		}
 	}
 }
